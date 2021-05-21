9c3ecd11daba07c3ef28f96b9a0a38a0c71a80bd
LisaPenzareva <84184163+LisaPenzareva@users.noreply.github.com>
2021-05-21 15:51:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
-/*jj*/
+//fpp//
 
 ReactDOM.render(<App />, document.getElementById("root"));
